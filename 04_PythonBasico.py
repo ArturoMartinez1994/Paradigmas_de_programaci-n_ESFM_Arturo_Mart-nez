59745f313c1d3bdf412a160047ca76cca4d0c469
ArturoMartinez1994 <143755655+ArturoMartinez1994@users.noreply.github.com>
2023-09-14 15:54:33 -0600

    04_PythonBasico.py
    Listas python

---
+++ 04_PythonBasico.py
@@ -0,0 +1,30 @@
+#=========================================
+# Arturo Martínez Espinosa
+#=========================================
+# Matemática Algorítmica
+# Paradigmas de Programación
+# ESFM IPN Septiembre 2023
+#=========================================
+
+#=========================================
+# Conjunto den python
+#=========================================
+even_nums ={2,4,6,8,10}  # conjunto de números pares 
+print(even_nums)
+
+# El bool no es parte del conjunto
+emp = {1, 'Steve', 10.5, True} #conjunto de diferentes objetos
+print (emp)
+
+nums = {1, 2, 2, 3, 4, 4, 5, 5}
+print (nums)
+
+#============================================
+# Convertir secuencia a conjunto
+# No lo genera en orden
+#============================================
+s = set('Hola')
+print(s)
+s = set((1,2,3,4,5)) # tupla a conjnto
+print(s)
+
