96bc7090567038f84b8589d8a0bd42ed7411676a
ArturoMartinez1994 <143755655+ArturoMartinez1994@users.noreply.github.com>
2023-10-26 15:48:49 -0600

    12_AtraparErrores.py
    Se necesita el programa cliente_bancario.py para correrlo correctamente.

---
+++ 12_AtraparErrores.py
@@ -0,0 +1,48 @@
+#===========================
+# Arturo Martínez Espinosa
+#===========================
+# Matemática Algorítmica
+# Paradigmas de Programación
+# ESFM IPN octubre 2023
+#===========================
+
+#======================================================
+# La clase ClienteBanacrio está en el subdirectorio
+# aplicaciion/banco/
+#======================================================
+from aplicacion.banco.cliente_bancario import ClienteBancario
+
+#=============================================
+# try: intenta (correr las instrucciones)
+# except: atrapar el error en una variable e
+# e se puede convertir a string
+#=============================================
+
+#===========================================
+# Error por sacar más dinero del que tiene
+#===========================================
+try:
+  cliente = ClienteBancario("Jaime Andrade", "Hernández Sánchez",28,0.0)
+  cliente.guardarDinero(300)
+  print(cliente.imprimirInfo())
+  cliente.retirarDinero(400)
+  print(cliente.imprimirInfo())
+#===========================================
+# Exception es el objeto más general de error
+#===========================================
+except Exception as e:
+  print("Error: " + str(e))
+
+#============================================
+# Error por usar un atributo privado
+#============================================
+try:
+  print(cliente.__nombres)
+except Exception as ex:
+  print("Error: " + str(ex))
+
+#=====================
+# Forma correcta
+#=====================
+print(cliente.nombres)
+
