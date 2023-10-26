c062aa576a96a90243e7961bdcaed63e11d51b6a
ArturoMartinez1994 <143755655+ArturoMartinez1994@users.noreply.github.com>
2023-10-26 15:50:43 -0600

    funciones.py

---
+++ funciones.py
@@ -0,0 +1,11 @@
+#=====================================
+# Funciones suma, promedio y potencia
+#=====================================
+def suma(x,y):
+    return x+y
+
+def promedio(x,y):
+    return 0.5*(x+y)
+
+def potencia(x,y):
+    return x**y
