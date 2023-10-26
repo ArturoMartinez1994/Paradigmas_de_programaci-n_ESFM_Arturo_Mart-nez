02f2d0e8a0ce51cdec1aef43ee06d03cc412a0d7
ArturoMartinez1994 <143755655+ArturoMartinez1994@users.noreply.github.com>
2023-10-26 15:50:25 -0600

    13_UtilizarFunciones.py
    Se necesita el programa funciones.py para correrlo correctamente.

---
+++ 13_UtilizarFunciones.py
@@ -0,0 +1,6 @@
+#===================================================
+# De funciones.py traer la funciones suma y promedio
+#===================================================
+from funciones import suma,promedio
+print(suma(100,200))
+print(promedio(100,200))
